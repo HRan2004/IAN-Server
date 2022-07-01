8ddb051fb3db2dd82f6f2c4c8b64bcfe9d3677f7
HRan2004 <2125764918@qq.com>
2022-07-02 02:29:42 +0800

    refactor(mapper): 使用注解SQL语句代替在资源处申明

--- src/main/kotlin/com/hraps/ianserver/mapper/AppMapper.kt
+++ src/main/kotlin/com/hraps/ianserver/mapper/AppMapper.kt
@@ -2,10 +2,13 @@ package com.hraps.ianserver.mapper
 
 import com.hraps.ianserver.entity.App
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
 import org.springframework.web.bind.annotation.GetMapping
 
 @Mapper
 interface AppMapper : BaseMapper<App> {
+
+    @Select("SELECT * FROM app WHERE name = #{name}")
     @GetMapping
     fun selectByName(name: String): App?
 
--- src/main/kotlin/com/hraps/ianserver/mapper/CardMapper.kt
+++ src/main/kotlin/com/hraps/ianserver/mapper/CardMapper.kt
@@ -8,13 +8,16 @@ import org.apache.ibatis.annotations.Update
 @Mapper
 interface CardMapper: BaseMapper<Card> {
 
+    @Select("SELECT * FROM card WHERE value = #{value} AND aid = #{aid}")
     fun selectOneByValueAndAid (value: String, aid: Int): Card?
 
+    @Select("SELECT * FROM card WHERE aid = #{aid} ORDER BY id DESC")
     fun selectByAid (aid: Int): List<Card>
 
     @Update("UPDATE card SET status = #{status} WHERE id = #{id}")
     fun updateStatusById (id: Int, status: Int): Integer
 
+    @Select("SELECT * FROM card WHERE value = #{value}")
     fun selectOneByValue(card: String): Card?
 
 }
